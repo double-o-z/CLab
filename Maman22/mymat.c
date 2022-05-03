d844ac88608714bfd72e7b28459ac25bd94ed03f
doubleoz <or.ohev.zion@gmail.com>
2022-05-03 05:10:29 -0700

    Create files and init matrices.

--- Maman22/mymat.c
+++ Maman22/mymat.c
@@ -1,2 +1,14 @@
 #include "mymat.h"
 
+/* utility function that prints all values of the given matrix.
+ * takes pointer for matrix, and prints all of its values, each row in a separate line. */
+void printMat(mat *MAT){
+    int i, j;
+    printf("MAT:\n");
+    for (i = 0; i < 4; ++i) {
+        for (j = 0; j < 4; ++j){
+            printf("%f ", MAT->arr[i][j]);
+        }
+        putchar('\n');
+    }
+}
--- Maman22/mymat.h
+++ Maman22/mymat.h
@@ -1,2 +1,7 @@
 #include <stdio.h>
 
+typedef struct {
+    float arr[4][4];
+} mat;
+
+void printMat(mat *MAT);
