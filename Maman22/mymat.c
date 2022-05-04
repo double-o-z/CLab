646f82fb6fa91b16cbdeb9e6cb8cb77d0dd74f18
doubleoz <or.ohev.zion@gmail.com>
2022-05-04 08:59:33 -0700

    Update.

--- Maman22/mymat.c
+++ Maman22/mymat.c
@@ -5,6 +5,9 @@ int main() {
     mat MAT_A, MAT_B, MAT_C, MAT_D, MAT_E, MAT_F;
     mat *mats[] = {&MAT_A, &MAT_B, &MAT_C, &MAT_D, &MAT_E, &MAT_F};
     char c;
+    char *line;
+    char *keep;
+    char *command;
 
     /* Initialize matrices with 0.0 values. */
     initMats(mats);
@@ -14,11 +17,16 @@ int main() {
 
     /* Get input from user, and while input is received (one line at a time), do commands. */
     while ((c = getchar()) != EOF){
-        if (isspace(c))
-            while (isspace(c = getchar()))
-                ; /* skip preceding white spaces. */
-        /* parse command name */
-        
+        do {
+            *line++ = c; /* Gather characters for a command. */
+        } while ((c = getchar()) != '\n');
+        *line = '\0';
+        /* Now we have a command, lets parse it. */
+        /* Separate by delim " "
+         * We should check number of results:
+         * 1. if result == "stop", do stop().
+         * 2. else separate by delim ","
+         *  2.1 then check matrix string by metrices array.*/
     }
     return 0;
 }
